1a6c962af577a557beb55e6834f6120e951b2e60
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-27 16:35:25 +0200

    update health
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,5 +3,5 @@ class Expense < ApplicationRecord
   belongs_to :building
   validates :user, uniqueness: { scope: :building }
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  validates :category, presence: true, inclusion: { in: %w[healt shopping hobbies food travel regular-expenses income] }
+  validates :category, presence: true, inclusion: { in: %w[health shopping hobbies food travel regular-expenses income] }
 end
