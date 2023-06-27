2970dedf02395fb867b5807b0b89183d8a36f5f0
Leslie Rados <leslie.rados@gmail.com>
2023-06-27 16:20:18 +0200

    nouevelle seed pour buildings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Building.destroy_all
+
 buildings = [{ name: 'Rastaurant', category: 'food', level: 2 },
-             { name: 'Clothes', category: 'shop', level: 3 },
+             { name: 'Clothes', category: 'shopping', level: 3 },
              { name: 'Hospital', category: 'health', level: 1 }]
 
 buildings.each do |building|
-  Bulding.create!(
+  Building.create!(
     name: building[:name],
     category: building[:category],
     level: building[:level]
