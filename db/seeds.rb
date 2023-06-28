a5f0ee059ac01118bffd98c10106b2d7c51be757
Leslie Rados <leslie.rados@gmail.com>
2023-06-28 10:48:33 +0200

    new branch challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-Expense.create!(name: "Rent", amount: 1000, date: "2021-09-01")
+# Expense.create!(name: "Rent", amount: 1000, date: "2021-09-01")
 
 Building.destroy_all
+Challenge.destroy_all
 
-buildings = [{ name: 'Rastaurant', category: 'food', level: 2 },
+buildings = [{ name: 'Restaurant', category: 'food', level: 2 },
              { name: 'Clothes', category: 'shopping', level: 3 },
              { name: 'Hospital', category: 'health', level: 1 }]
 
@@ -22,3 +15,16 @@ buildings.each do |building|
   )
 end
 
+challenges = [{ title: 'No restaurant', description: 'No restaurant for 1 week', badge: 'No restaurant', level: 1 },
+              { title: 'No shopping', description: 'No shopping for 2 weeks', badge: 'No shopping', level: 2 },
+              { title: 'Supersaver', description: 'No useless dispenses for 3 weeks', badge: 'Savings', level: 3 },
+              { title: 'Good health', description: 'You did not go to hospital for a month!', badge: 'Good health', level: 1 }]
+
+challenges.each do |challenge|
+  Challenge.create!(
+    title: challenge[:title],
+    description: challenge[:description],
+    badge: challenge[:badge],
+    level: challenge[:level]
+  )
+end
