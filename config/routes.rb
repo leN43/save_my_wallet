5b69a0d253d1e352f439abc6392145011b914de1
Niels Ayigbede <niels.ayigbede@gmail.com>
2023-06-28 16:52:28 +0200

    cityController + routes +views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
 
-  resources :cities, only: %i[index show update edit destroy]
+  resources :cities, only: %i[show update edit destroy]
   resources :challenges, only: %i[index show]
   resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
