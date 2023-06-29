d270ad7cc3abb501b39e41295fda66819bb968e4
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-29 17:50:33 +0200

    update button
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,3 @@
 import "bootstrap"
 import "@popperjs/core"
 import "controllers"
-
-
-// TO Change in a controller once city page is created
-$(document).ready(function(){
-  $('.close').click(function(){
-    $('ul').toggleClass('active');
-  })
-})
