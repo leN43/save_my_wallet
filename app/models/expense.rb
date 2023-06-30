af805b5cc697e9429479dcafca29e3828a01cc3b
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-30 12:01:18 +0200

    add form for expenses
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :building
-  validates :user, uniqueness: { scope: :building }
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :category, presence: true, inclusion: { in: %w[health shopping hobbies food travel regular-expenses income] }
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
