c5cfc038e100ccf96a0864a27f4c9fbaa860a57e
Quentin Ribardière quentin.rbd@gmail.com <quentin.rbd@gmail.com>
2023-07-04 14:46:27 +0200

    user_game fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,13 @@ module ApplicationHelper
     }
   end
 
+  def achieved_or_not
+    {
+      true => "#{icon_class('check')}",
+      false => "#{icon_class('xmark')}"
+    }
+  end
+
   private
   def icon_class(icon)
     "fa-solid fa-#{icon}"
